bdd6c6ca9ed582cfd4b2a292437810845d19f147
Alex Tucker <alex@floop.org.uk>
2017-03-28 11:48:41 +0100

    Added Apache 2.0 license and detailed dependencies.

--- vis/app.js
+++ vis/app.js
@@ -1,3 +1,19 @@
+/*
+   Copyright 2017 Alex Tucker
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,7 +25,6 @@ var proxy = require('http-proxy-middleware');
 var urlparse = require('url');
 
 var topicvis = require('./routes/topicvis');
-var onsdatavis = require('./routes/onsdatavis');
 var fdpvis = require('./routes/fdpvis');
 
 /* var auth = function (req, res, next) {
